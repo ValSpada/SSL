7bb97025f90a20d9b577aef3f8e4c6e5e8471d4c
ValSpada <vspada@frba.utn.edu.ar>
2025-04-08 14:06:52 -0300

    Redo and finished

--- 00-CHelloWorld/hello.c
+++ 00-CHelloWorld/hello.c
@@ -1,15 +1,11 @@
 /* hello.c
  * My first hello world !
  * Valentín Francisco Spada
- * 2025-04-02
+ * 2025-04-08
 */
 
 #include <stdio.h>
 
 int main(){
-    FILE *salida;
-
-    salida = fopen("output.txt", "w");
-    fputs("Hello, world!", salida);
-    fclose(salida);
+    printf("Hello, world!\n");
 }
